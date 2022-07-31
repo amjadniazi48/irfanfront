8266f3c73512092a6dbcc59cb776e2eb389241f4
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2022-08-01 00:17:57 +0500

    changes in categoryItem

--- components/CategoryItem.js
+++ components/CategoryItem.js
@@ -34,7 +34,7 @@ const ArticleItem = ({ item }) => {
 
             </a>
           </Link>
-          <figcaption>{item.attributes.title}</figcaption>
+         
         </div>
       </div>
       <div className="col-sm-12 col-md-12 col-lg-12">
