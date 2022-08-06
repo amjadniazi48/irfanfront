e98529c0473ed7b68088b4a1a282730ef5e61dd8
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2022-08-07 00:24:12 +0500

    update urdu for cats

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -13,13 +13,13 @@ const loadData = async (locale) => {
   const { locale, locales } = useRouter();
   let className='';
   const { data } = useSWR([locale, "categoires"], loadData);
+  console.log(data)
   if(locale=="en"){className='list-unstyled sidebar-link dark before-caret-dark custom-category'} else {className='list-unstyled sidebar-link dark  custom-category'}
   return (
     <aside className="col-md-4 widget-area end-sidebar-lg" id="right-sidebar"  dir={locale === 'ur-PK' ? 'rtl' : 'ltr'}>
      <ProfileSidebar profileData={profileData}/>
       <aside id="categories_custom-1" className="widget widget_categories widget_categories_custom" >
-        <div className="block-title-4">
-          <h4 className="h5 title-arrow"><span>{locale=='ur-PK'?'زمرہ جات ':'Categories'}</span></h4>
+        <div className="block-title-4"><h4 className="h5 title-arrow"><span>{locale=='ur-PK'?'زمرہ جات ':'Categories'}</span></h4>
         </div>
         <ul className={className} style={locale=='en'?{ 'listStyle':'none'}:{'listStyle':'circle '}}>
           {data && data.map((item) => {
