a6a7f92a0baa0acb6ff0667fc5abcef9837eb176
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2022-09-05 21:54:04 +0500

    Added quill editor

--- pages/posts/add.js
+++ pages/posts/add.js
@@ -11,8 +11,11 @@ import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import { useForm } from "react-hook-form";
 import { API_URL } from "@/config/index";
+import dynamic from 'next/dynamic';
 
+import 'react-quill/dist/quill.snow.css';
 export default function AddPostPage({ token }) {
+    const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
     const { t, lang } = useTranslation('common')
     const [title, setTitle] = useState("");
     const [show, setShow] = useState(false);
@@ -20,6 +23,9 @@ export default function AddPostPage({ token }) {
     const [description, setDescription] = useState("");
     const { locale, locales } = useRouter();
     const { register, handleSubmit, formState: { errors } } = useForm();
+    const onEditorStateChange = (editorState) => {
+        setDescription("description", editorState);
+      };
     const onSubmit = async (e) => {
         const myPost = { data: { title, slug, description,locale} }
         const requestOptions = {
@@ -73,13 +79,11 @@ export default function AddPostPage({ token }) {
                                         {errors.slug && <p className="text-danger">Slug is required</p>}
                                     </Form.Group>
                                     <Form.Group className="mb-3" controlId="formBasicPassword">
-                                        <Form.Control
-                                            as="textarea"
-                                            placeholder={t("common:formDescripton")}
-                                            style={{ height: '400px' }}
-                                            value={description}
-                                            {...register("description", { required: true })}
-                                            onChange={(e) => setDescription(e.target.value)}
+                                    <ReactQuill
+                                            theme="snow"
+                                          
+                                            placeholder="تفصیل"
+                                            onChange={onEditorStateChange}
 
                                         />
                                         {errors.title && <p className="text-danger">Description is required</p>}
